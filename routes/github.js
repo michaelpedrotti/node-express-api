1d8250ce1bbbd9d91ba444f89347e1173fc9af08
Michael Pedrotti <michael.pedrotti.11@gmail.com>
2023-04-14 08:18:46 -0300

    Authentication and Autorization to github routes

--- routes/github.js
+++ routes/github.js
@@ -1,6 +1,8 @@
+const { isAuthenticated } = require('../middlewares/authenticationMiddleware');
+const { isAuthorized } = require('../middlewares/authorizationMiddleware');
 const Controller = require('../controllers/githubUserController');
 
 module.exports = require('express').Router()
-    .get('/users/:username/details', Controller.detail)    
-    .get('/users/:username/repos', Controller.repos)
-    .get('/users', Controller.index);
+    .get('/users/:username/details', isAuthenticated, isAuthorized('github', 'read'), Controller.detail)    
+    .get('/users/:username/repos', isAuthenticated, isAuthorized('github', 'read'), Controller.repos)
+    .get('/users', isAuthenticated, isAuthorized('github', 'read'), Controller.index);
